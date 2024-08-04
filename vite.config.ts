3046c904105a0e18b3c86d91e43a0db86e793eb9
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-08-04 21:35:23 +0545

    final-commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,16 +9,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       '/api': 'https://login.dataconstruct.com.np',
-//     },
-//   },
-//   plugins: [react()],
-// })
-
 export default defineConfig({
   server: {
     proxy: {
